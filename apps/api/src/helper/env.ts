623820b9bf5da3577ec5328c84fa70fe9bd04fae
ymkz <ymkz@users.noreply.github.com>
2025-03-04 08:21:29 +0900

    refactor: 環境変数のコメントを日本語に翻訳し、説明を追加

--- apps/api/src/helper/env.ts
+++ apps/api/src/helper/env.ts
@@ -5,11 +5,11 @@ export const env = createEnv({
 	runtimeEnv: process.env,
 	emptyStringAsUndefined: true,
 	server: {
-		// defaults
+		// defaults :: 環境変数で上書き可能
 		APP_PORT: z.optional(z.string().transform(Number).pipe(z.number().int())).default('8080'),
 		LOG_LEVEL: z.enum(['debug', 'info', 'warn', 'error', 'silent']).default('info'),
 
-		// configs
+		// configs :: `.env.{envionment}`から設定される
 		NODE_ENV: z.enum(['development', 'production', 'test']),
 		APP_ENV: z.enum(['local', 'test', 'alpha', 'beta', 'live']),
 		HEALTHCHECK: z.enum(['UP', 'DOWN']),
@@ -17,7 +17,7 @@ export const env = createEnv({
 		MYSQL_USER: z.string(),
 		MYSQL_DATABASE: z.string(),
 
-		// credentials
+		// credentials :: 実行環境で環境変数として設定する必要があるクレデンシャル
 		MYSQL_PASSWORD: z.string(),
 	},
 });
